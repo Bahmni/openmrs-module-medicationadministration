0cdd91ecadc51a34db29fe0de112d3fc9d1640a9
Kavitha S <kavitha.s@thoughtworks.com>
2023-12-29 01:30:58 +0530

    Kavitha | deleted test file

--- api/src/test/java/org/openmrs/BaseIntegrationTest.java
+++
@@ -1,6 +0,0 @@
-//package org.openmrs;
-//import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
-//
-//@org.springframework.test.context.ContextConfiguration(locations = {"classpath:TestingApplicationContext.xml"})
-//public abstract class BaseIntegrationTest extends BaseModuleWebContextSensitiveTest {
-//}
